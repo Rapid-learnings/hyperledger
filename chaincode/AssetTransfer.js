7e43e76de9c1577111aee0fd447febdb08ee928e
Ubuntu <ubuntu@ip-172-31-23-222.us-east-2.compute.internal>
2022-10-10 07:51:03 +0000

    Added smart contract & made changes to cli pah yaml file

--- chaincode/AssetTransfer.js
+++ chaincode/AssetTransfer.js
@@ -14,4 +14,14 @@ class AssetTransfer extends Contract{
         }
         await ctx.stub.putState(id, Buffer.from(JSON.stringify(asset)));
     }
+    
+    // ReadAsset returns the asset stored in the world state with given id.
+    async ReadAsset(ctx, id) {
+        const assetJSON = await ctx.stub.getState(id); // get the asset from chaincode state
+        if (!assetJSON || assetJSON.length === 0) {
+            throw new Error(`The asset ${id} does not exist`);
+        }
+        return assetJSON.toString();
+    }
+
 }
