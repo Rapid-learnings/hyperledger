604a5c296541f3fa0c84ac01c79bda78c70a839e
Ubuntu <ubuntu@ip-172-31-23-222.us-east-2.compute.internal>
2022-10-11 08:32:17 +0000

    Added final steps, corrected yaml files

--- chaincode/AssetTransfer.js
+++ chaincode/AssetTransfer.js
@@ -4,6 +4,91 @@ class AssetTransfer extends Contract{
     // this class provides tx context for 
     // fx defined within the smart contract
 
+    async InitLedger(ctx) {
+        const assets = [
+            {
+                ID: 'asset1',
+                Color: 'blue',
+                Size: 5,
+                Owner: 'Tomoko',
+                AppraisedValue: 300,
+            },
+            {
+                ID: 'asset2',
+                Color: 'red',
+                Size: 5,
+                Owner: 'Brad',
+                AppraisedValue: 400,
+            },
+            {
+                ID: 'asset3',
+                Color: 'green',
+                Size: 10,
+                Owner: 'Jin Soo',
+                AppraisedValue: 500,
+            },
+            {
+                ID: 'asset4',
+                Color: 'yellow',
+                Size: 10,
+                Owner: 'Max',
+                AppraisedValue: 600,
+            },
+            {
+                ID: 'asset5',
+                Color: 'black',
+                Size: 15,
+                Owner: 'Adriana',
+                AppraisedValue: 700,
+            },
+            {
+                ID: 'asset6',
+                Color: 'white',
+                Size: 15,
+                Owner: 'Michel',
+                AppraisedValue: 800,
+            },
+        ];
+
+        for (const asset of assets) {
+            asset.docType = 'asset';
+            await ctx.stub.putState(asset.ID, Buffer.from(JSON.stringify(asset)));
+            console.info(`Asset ${asset.ID} initialized`);
+        }
+    }
+
+    // CreateAsset issues a new asset to the world state with given details.
+    async CreateAsset(ctx, id, color, size, owner, appraisedValue) {
+        const asset = {
+            ID: id,
+            Color: color,
+            Size: size,
+            Owner: owner,
+            AppraisedValue: appraisedValue,
+        };
+        await ctx.stub.putState(id, Buffer.from(JSON.stringify(asset)));
+        return JSON.stringify(asset);
+    }
+    async GetAllAssets(ctx) {
+        const allResults = [];
+        // range query with empty string for startKey and endKey does an open-ended query of all assets in the chaincode namespace.
+        const iterator = await ctx.stub.getStateByRange('', '');
+        let result = await iterator.next();
+        while (!result.done) {
+            const strValue = Buffer.from(result.value.value.toString()).toString('utf8');
+            let record;
+            try {
+                record = JSON.parse(strValue);
+            } catch (err) {
+                console.log(err);
+                record = strValue;
+            }
+            allResults.push({ Key: result.value.key, Record: record });
+            result = await iterator.next();
+        }
+        return JSON.stringify(allResults);
+    }
+
     async AssetTransfer(ctx, id, color, owner, value){
         const hondaAsset = {
             ID: id,
@@ -14,14 +99,7 @@ class AssetTransfer extends Contract{
         }
         await ctx.stub.putState(id, Buffer.from(JSON.stringify(asset)));
     }
-    
-    // ReadAsset returns the asset stored in the world state with given id.
-    async ReadAsset(ctx, id) {
-        const assetJSON = await ctx.stub.getState(id); // get the asset from chaincode state
-        if (!assetJSON || assetJSON.length === 0) {
-            throw new Error(`The asset ${id} does not exist`);
-        }
-        return assetJSON.toString();
-    }
 
 }
+
+module.exports = AssetTransfer;
