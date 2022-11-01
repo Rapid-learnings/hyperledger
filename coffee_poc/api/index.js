ba0b9577484ac15c6c440193739b5dd7b6ebdccc
Ubuntu <ubuntu@ip-172-31-23-222.us-east-2.compute.internal>
2022-11-01 12:57:40 +0000

    Added fabric-ca server/ client related files

--- coffee_poc/api/index.js
+++ coffee_poc/api/index.js
@@ -1,10 +1,11 @@
 // index.js
 'use strict';
 var { Gateway, Wallets } = require('fabric-network');
-const fs = require('fs');
+const fs = require('fs');``
 const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const path = require('path');
 
 const app = express();
 // app.use(bodyParser);
@@ -31,6 +32,8 @@ app.post('/register/admin', async(req,res,next)=>{
 app.post('/register/user', async(req,res,next)=>{
     let usrname = req.body.username;
     let orgName = req.body.orgName;
+    console.log(orgName);
+    console.log(usrname);
     let resp = registerUser.registerEnrollUser(usrname,orgName);
     res.json(resp);
 })
@@ -46,27 +49,20 @@ app.post('/register/user', async(req,res,next)=>{
 
 
 app.get('/getStorage', async(req,res,next)=>{
-    try{
-        let ccp = JSON.parse(fs.readFile("./connection-profiles/mfc-prd-config.json",'utf8'))
-        const walletPath = path.join(process.cwd(),'teafarm-wallet');
-        const wallet = await Wallets.newFileSystemWallet(walletPath)
-        // omitting check for identity
-        const gateway = new Gateway();
-        await gateway.connect(ccp, {wallet, identity:'', discovery:{enabled:true, asLocalhost:false}})
-        
-        // n/w to which contract is deployed
-        const network = await gateway.getNetwork('mfd-prd-channel')
-        // get contract
-        const contract = network.getContract('mfcPrd.js')
-        const storage = await contract.evaluateTransaction('getStorage');
+    let ccp = fs.readFile("./connection-profiles/mfc-prd-config.json",'utf8',function(err, data){})
+    const walletPath = path.join(process.cwd(),'teafarm-wallet');
+    const wallet = await Wallets.newFileSystemWallet(walletPath)
+    // omitting check for identity
+    const gateway = new Gateway();
+    await gateway.connect(ccp, {wallet, identity:'user1', discovery:{enabled:true, asLocalhost:false}})
+    
+    // n/w to which contract is deployed
+    const network = await gateway.getNetwork('mfd-prd-channel')
+    // get contract
+    const contract = network.getContract('mfcPrd.js')
+    const storage = await contract.evaluateTransaction('getStorage');
 
-        res.json(storage);
-    }catch(err){
-        throw err;
-    }
-    // await manufactuererProducer.setInitialStorageForProducer();
-    // let storage = await manufactuererProducer.getStorageFromProducer();
-    // return res.json({message:"Storage is "+storage})
+    res.json(storage);
 });
 
 
