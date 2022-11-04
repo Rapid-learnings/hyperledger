101f71e59fdb1c01b03cc55de559244321ed3df2
Muhammed rasheed <rasheed@rapidinnovation.dev>
2022-11-04 15:55:50 +0530

    some changes

--- coffee/chaincode/pm-cc.js
+++ coffee/chaincode/pm-cc.js
@@ -5,17 +5,20 @@ SPDX-License-Identifier: Apache-2.0
 'use strict';
 
 const { Contract } = require('fabric-contract-api');
-const { TokenERC20Contract } = require('./token-erc-20/chaincode-javascript/lib/tokenERC20.js')
+const { ClientIdentity } = require('fabric-shim');
+// const { TokenERC20Contract } = require('./token-erc-20/chaincode-javascript/lib/tokenERC20.js')
 const stockKey = 'stock'
 const orderKey = 'orderNumber'
 const Status = ['order-placed', 'in-transit', 'delivered', 'payout-claimed']
+const manufacturerBalanceKey = 'MANUFACTURER_BALANCE'
+const producerBalanceKey = 'PRODUCER_BALANCE'
 
 class pmcc extends Contract {
 
     // initializes the stock of the producer to a set amount and also initializes the erc20 token contract
     async initialize(ctx) {
         const currentStockBytes = await ctx.stub.getState(stockKey);
-        await TokenERC20Contract.initialize(ctx, "token", "TOK", 18);
+        // await TokenERC20Contract.initialize(ctx, "token", "TOK", 18);
         if (currentStockBytes || currentStockBytes.length > 0) {
             throw new Error('Contract already initialised');
         }
@@ -24,13 +27,36 @@ class pmcc extends Contract {
         console.log("====== Initial Coffee Stock Set To 100 Kg ======");
     }
 
-    // amt - int
+    // fucntion for initializing the balances for the manufacturer
+    async initializeBalanceForManufacturer(ctx) {
+        const clientMSP = await ctx.clientIdentity.getMSPID();
+        if (clientMSP !== 'tataMSP') {
+            throw new Error('Only Manufacturer can initialize balance');
+        }
+        // storing the balance of 1000000 $ with the manufacturer MSP ID as key
+        console.log('Balance of tataMSP initialized to 1000000 $');
+        await ctx.stub.putState(manufacturerBalanceKey, Buffer.from('1000000'));
+    }
+
+    // fucntion for initializing the balances for the manufacturer
+    async initializeBalanceForProducer(ctx) {
+        const clientMSP = await ctx.clientIdentity.getMSPID();
+        if (clientMSP !== 'teafarmMSP') {
+            throw new Error('Only Manufacturer can initialize balance');
+        }
+        // storing the balance of 1000000 $ with the manufacturer MSP ID as key
+        console.log('Balance of teafarmMSP initialized to 0 $');
+        await ctx.stub.putState(producerBalanceKey, Buffer.from('0'));
+    }
+
     // this function updates the stock in production.
     async updateStock(ctx, amtInKg, flag) {
         const clientMSPID = await ctx.clientIdentity.getMSPID();
         if (clientMSPID !== 'teafarmMSP') {
             throw new Error('only producer can update stock')
         }
+
+        // Fetching current stock
         const currentStockBytes = await ctx.stub.getState(stockKey);
         let updatedStock;
         if (!currentStockBytes || currentStockBytes.length === 0) {
@@ -78,7 +104,7 @@ class pmcc extends Contract {
         let stock
         try {
             stock = await this.updateStock(ctx, qty, 0);
-        } catch(err) { 
+        } catch (err) { 
             throw err;
         }
 
@@ -109,6 +135,15 @@ class pmcc extends Contract {
         // await TokenERC20Contract.Approve()
         // await TokenERC20Contract.TransferFrom(ctx, clientMSPID, this.toString(), amt)
 
+        // Updating the balances of the manufacturer
+        const balanceBytes = await ctx.stub.getState(manufacturerBalanceKey);
+        const balance = parseInt(balanceBytes.toString());
+        // Check for insufficient funds
+        if (balance < amt) {
+            throw new Error('Insufficient funds');
+        }
+        const newBalance = balance - amt;
+        await ctx.stub.putState(manufacturerBalanceKey, Buffer.from(newBalance.toString()))
         // store the order details in the blockchain with the orderNo as key
         await ctx.stub.putState(orderNo, Buffer.from(JSON.stringify(order)));
     }
@@ -169,20 +204,28 @@ class pmcc extends Contract {
 
         // check whether status is 'delivered' else throws error
         if (status === Status[2]) {
-            const orderObjBytes = await ctx.stub.getState(orderNo);
-            const orderObj = parse(JSON.stringify(orderObjBytes));
-            const amt = orderObj.Amount;
+            amt = orderObj.Amount;
+            // transferring amount to producer
+            let balance;
+            try {
+                balance = await this.getBalance(ctx);
+            } catch (err) {
+                throw err;
+            }
+            const newBalance = balance + amt;
+            await ctx.stub.putState(producerBalanceKey, Buffer.from(newBalance.toString()));
             // await TokenERC20Contract.transferFrom(ctx, this.toString(), clientMSP, amt)
+
+            // updating the status of the order to payout-claimed
+            orderObj.Status = Status[3];
+
+            //storing the new order details object with the orderNo key
+            await ctx.stub.putState(orderNo, Buffer.from(JSON.stringify(orderObj)));
+            console.log('====== Payout of %s $ has been claimed ======', amt);
+            return amt;
         } else {
             throw new Error('shipment must be delivered in order to collect payout');
         }
-
-        // updating the status of the order to payout-claimed
-        orderObj.Status = Status[3];
-        //storing the new order details object with the orderNo key
-        await ctx.stub.putState(orderNo, Buffer.from(JSON.stringify(orderObj)));
-        console.log('====== Payout of %s has been claimed ======', amt);
-        return amt;
     }
 
     // Queries the order details object with orderNo as key
@@ -195,6 +238,22 @@ class pmcc extends Contract {
         console.log("Order quantity is %s Kg", orderObj.Quantity);
         console.log("Current status of order is %s", Status);
         return orderObj;
+    }
+
+    // function to check account balances in $ of the user
+    async getBalance(ctx) {
+        const clientMSP = await ctx.clientIdentity.getMSPID();
+        if (clientMSP === 'teafarmMSP') {
+            const balanceBytes = await ctx.stub.getState(producerBalanceKey);
+            const balance = parseInt(balanceBytes.toString());
+            console.log('Balance for %s is %s $', clientMSP, balance);
+            return balance;
+        } else {
+            const balanceBytes = await ctx.stub.getState(manufacturerBalanceKey);
+            const balance = parseInt(balanceBytes.toString());
+            console.log('Balance for %s is %s $', clientMSP, balance);
+            return balance;
+        }
     }
 }
 
